0d6c4e5a596075a1d5f615fd3bb25387b7c75028
Hardik Sharma <44233098+MPL0Y@users.noreply.github.com>
2020-10-18 16:38:04 +0530

    Update 1.c

---
+++ c/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+        You have to print a stair-like pattern.
+    
+        Example:
+        n = 4
+        #
+        ##
+        ###
+        ####
+        
+        n = 1
+        #
+    */
+    return 0;
+}
