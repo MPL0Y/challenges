e24f0861fd218e1a1d74f05b11ac1c13d00eabd2
Hardik Sharma <44233098+MPL0Y@users.noreply.github.com>
2020-10-18 16:20:26 +0530

    Update 2.py

--- python/2.py
+++ python/2.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 # Complete the function is_palindrome()
 # Should return True if string is a palindrome, else False
 
@@ -9,24 +11,24 @@ def is_palindrome(string):
 
 def count_unique(list):
     pass
-    
+
 # Complete the function remove_string()
 # Should return the string representation when all occurrences of string b is removed from string a
 
 def remove_string(a, b):
     pass
-    
+
 # You are not allowed to change the code below!
 # Run the following program on your machine
 # All tests should pass
 
 def check(a, b):
-    return "Passed!" if a == b else "Failed!"
+    pass
 
 if __name__ == "__main__":
     print(f"Test case 1: {check(is_palindrome(''), True)} [is_palindrome()]"); sleep(1.5)
-    print(f"Test case 2: {check(is_palindrome('MPL0Y/challenges is so cool! Isn't it?'), '?ti t'nsI !looc os si segnellahc/Y0LPM')} [is_palindrome()]"); sleep(1.5)
+    print(f"Test case 2: {check(is_palindrome('racecar'), True)} [is_palindrome()]"); sleep(1.5)
     print(f"Test case 3: {check(count_unique([1, 1]), 1)} [count_unique()]"); sleep(1.5)
-    print(f"Test case 4: {check(count_unique([1, 4, 2, 3, 99, 4, -1, 0, 13, 3, 5, 7, 6, -1, 12]), 13)} [count_unique()]"); sleep(1.5)
+    print(f"Test case 4: {check(count_unique([1, 2, 3, 4, 1, 2, 4, 5, 6, 5]), 6)} [count_unique()]"); sleep(1.5)
     print(f"Test case 5: {check(remove_string('', ''), '')} [remove_string()]"); sleep(1.5)
     print(f"Test case 6: {check(remove_string('abcbdbababcabacbabc', 'abc'), 'bdbababacb')} [remove_string()]"); sleep(1.5)
